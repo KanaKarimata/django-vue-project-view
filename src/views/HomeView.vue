2b4cc77f0e0b3bf5d3ea77ea851ff510843cdc6e
KanaKarimata <kana.lt22@gmail.com>
2024-05-12 18:06:00 +0900

    edit Home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Djacket
+        </p>
+        <p class="subtitle">
+          The best jacket store online
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+    </div>
+
+    <div
+      class="column is-3"
+      v-for="product in latestProducts"
+      :key="product.id"
+    >
+      <div class="box">
+        <figure class="image mb-4">
+          <img :src="product.get_thumbnail">
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+          View details
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
